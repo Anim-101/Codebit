b90f1040a4d10e12bb35f8640c9a942cb3e24a1f
Anim <Anim-101@users.noreply.github.com>
2020-09-03 10:09:53 +0600

    Update NumberRowSerialized.java

--- Code Snippets/Java/Pattern/NumberRowSerialized.java
+++ Code Snippets/Java/Pattern/NumberRowSerialized.java
@@ -3,19 +3,22 @@
 Problem ->
 
 Given n number of rows, show number serially from 1 to continuously until rows' count expire.
+
 Test Case:
 
-	n = 9
+	Input:	
+		n = 9
 	
-	1
-	2 3
-	4 5 6
-	7 8 9 10
-	11 12 13 14 15
-	16 17 18 19 20 21
-	22 23 24 25 26 27 28
-	29 30 31 32 33 34 35 36
-	37 38 39 40 41 42 43 44 45    
+	OutPut:
+		1
+		2 3
+		4 5 6
+		7 8 9 10
+		11 12 13 14 15
+		16 17 18 19 20 21
+		22 23 24 25 26 27 28
+		29 30 31 32 33 34 35 36
+		37 38 39 40 41 42 43 44 45    
 */
 
 
